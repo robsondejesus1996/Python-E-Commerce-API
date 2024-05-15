cb3f096c7e8dbdb8bc5dc235aa60f9b3356d291f
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-05-15 12:46:01 -0300

    feat: create login and logout auth

--- Project/app.py
+++ Project/app.py
@@ -1,11 +1,54 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "minha_chave_123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+CORS(app)
+
+
+
 
 # Modelagem
+# User(id, username, password)
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+
+#Autenticacao
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+# Rota de login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    
+
+    user = User.query.filter_by(username=data.get("username")).first()
+    
+    if user and data.get("password") == user.password:
+            login_user(user)
+            return jsonify({'message': "Logged in successfully"})
+    return jsonify({'message': "Unauthorized. Invalid credentials"}), 401
+
+
+#Rota de logout
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return  jsonify({'message': "Logout successfully"})
+
 # Produtp (id, name, preice, description)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +58,7 @@ class Product(db.Model):
 
 #Roda de adicionar um produto
 @app.route('/api/products/add', methods=["POST"])
+@login_required
 def add_product():
     data = request.json 
     if 'name' in data and 'price' in data:
@@ -26,6 +70,7 @@ def add_product():
 
 #Roda de remover produto
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     # Recuperar o produto da base de dados 
     product = Product.query.get(product_id)
@@ -34,11 +79,57 @@ def delete_product(product_id):
         db.session.commit()
         return  jsonify({'message': "Product deleted successfully"})
     return jsonify({'message': "Proct not found"}), 404
-        
 
-    # Verificar se o produto existe
-    # Se existe apagar da base de dados 
-    # se não existe retornar 404 not found 
+
+#Roda para recuperar o produto 
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+    return jsonify({"message": "Product not found"}), 404
+
+
+#Roda de Atualização de produto
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])      
+@login_required 
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+
+    if 'price' in data:
+        product.price = data['price']
+
+    if 'description' in data:
+        product.description = data['description']    
+
+    db.session.commit()
+
+    return jsonify({'message': 'Product updated successfully'})    
+
+#Roda de recuperar lista de produtos
+@app.route('/api/products', methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)    
 
 
 # Definir uma rota raiz(página inicial) e a função que será executada ao requisitar
